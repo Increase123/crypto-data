3a18678b0bfc63c80b86b6cbf4d2ba41884a88c3
Increase <increaseakinwole46@gmail.com>
2025-10-16 16:48:35 +0100

    Added the Footer section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import Analytics from "./components/Analytics";
 import Newsletter from "./components/Newsletter";
 import Cards from "./components/Cards";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Analytics />
       <Newsletter />
       <Cards />
+      <Footer />
     </>
   );
 };
