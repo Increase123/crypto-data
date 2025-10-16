dbac9a881e24f1f53d6904fd76e27f9acba7d9fb
Increase <increaseakinwole46@gmail.com>
2025-10-16 16:53:47 +0100

    Updated the footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,12 +10,13 @@ const Footer = () => {
   return (
     <div className="max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300">
       <div>
-        <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
+        <h1 className="w-full text-3xl font-bold text-[#00df9a]">CRYPTO.</h1>
         <p className="py-4">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id odit
           ullam iste repellat consequatur libero reiciendis, blanditiis
           accusantium.
         </p>
+
         <div className="flex justify-between md:w-[75%] my-6">
           <FaFacebookSquare size={30} />
           <FaInstagram size={30} />
@@ -23,9 +24,11 @@ const Footer = () => {
           <FaGithubSquare size={30} />
         </div>
       </div>
+
       <div className="lg:col-span-2 flex justify-between mt-6">
         <div>
           <h6 className="font-medium text-gray-400">Solutions</h6>
+
           <ul>
             <li className="py-2 text-sm">Analytics</li>
             <li className="py-2 text-sm">Marketing</li>
@@ -33,6 +36,7 @@ const Footer = () => {
             <li className="py-2 text-sm">Insights</li>
           </ul>
         </div>
+
         <div>
           <h6 className="font-medium text-gray-400">Support</h6>
           <ul>
@@ -42,6 +46,7 @@ const Footer = () => {
             <li className="py-2 text-sm">API Status</li>
           </ul>
         </div>
+
         <div>
           <h6 className="font-medium text-gray-400">Company</h6>
           <ul>
@@ -52,6 +57,7 @@ const Footer = () => {
             <li className="py-2 text-sm">Careers</li>
           </ul>
         </div>
+
         <div>
           <h6 className="font-medium text-gray-400">Legal</h6>
           <ul>
