60712b11b365b5716ba4d040f9f7595bd8685de7
Increase <increaseakinwole46@gmail.com>
2025-10-16 15:49:20 +0100

    Added Analytics section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Analytics from "./components/Analytics";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <Analytics />
     </>
   );
 };
